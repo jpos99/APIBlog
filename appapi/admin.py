9c3fffbe9a7d16d49a8b63ee555c0d8bdba3e192
Joao Pedro Soares <jpos99@hotmail.com>
2021-05-27 21:26:03 -0300

    Criando as classes e registrando os modelos no admin do django

--- appapi/admin.py
+++ appapi/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from appapi.models import *
 
-# Register your models here.
+
+class Postagens(admin.ModelAdmin):
+    list_display = ('id', 'titulo_postagem', 'resumo_postagem', 'corpo_postagem', 'autor_postagem', 'data_postagem')
+    list_display_links = ('titulo_postagem', 'resumo_postagem', 'corpo_postagem')
+    search_fields = ('id', 'titulo_postagem', 'autor_postagem', 'data_postagem')
+    list_per_page = 50
+
+
+admin.site.register(Postagem, Postagens)
+
+
+class Autores(admin.ModelAdmin):
+    list_display = ('id', 'nome_autor')
+    list_display_links = ('nome_autor',)
+    search_fields = ('id', 'nome_autor')
+    list_per_page = 50
+
+
+admin.site.register(Autor, Autores)
