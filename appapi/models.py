f58e4e55667434a510568489fe3702fc8f9bad13
Joao Pedro Soares <jpos99@hotmail.com>
2021-05-27 21:08:49 -0300

    Criando as tabelas (classes) no models.py

--- appapi/models.py
+++ appapi/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Autor(models.Model):
+    classifica = (
+        ('a', 'Diamante'),
+        ('b', 'Ouro'),
+        ('c', 'Prata')
+    )
+    nome_autor = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    classificacao_autor = models.CharField(max_length=1, choices=classifica, blank=False, null=False, default='c')
+    usuario_autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    foto_autor = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nome_autor
+
+
+class Postagem(models.Model):
+    titulo_postagem = models.CharField(max_length=100, unique=True)
+    resumo_postagem = models.TextField(max_length=400)
+    corpo_postagem = models.TextField()
+    autor_postagem = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    data_postagem = models.DateTimeField(auto_now=True, editable=False)
+    atualizacao_postagem = models.DateTimeField(auto_now_add=True, editable=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.titulo_postagem
+
+
+class Log(models.Model):
+    usuario_log = models.ForeignKey(User, on_delete=models.DO_NOTHING, editable=False)
+    acao_log = models.TextField(editable=False, null=False, blank=False)
+    data_log = models.DateTimeField(auto_now=True, null=False, blank=False)
